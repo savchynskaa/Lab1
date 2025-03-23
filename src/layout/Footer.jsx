66f3183c417100024a4a2b9e60e9ee0318bfd8b5
Andriana Savchynska <savchynska2202@gmail.com>
2025-03-23 20:15:03 +0200

    Initial commit

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,21 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className='page-footer blue lighten-2'>
+                <div className='footer-copyright'>
+                    <div className='container'>
+                        © 2022 Copyright Text
+                        <a className='grey-text text-lighten-4 right' href='https://github.com/Sbavdik/react-movies' target="blank" rel="noreferrer">Repo</a>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+
+
+
+export default Footer;
---
+++ src/layout/Header.jsx
@@ -0,0 +1,17 @@
+
+
+const Header = () => {
+    return (
+        <nav className='blue'>
+            <div className='nav-wrapper'>
+                <a href='https://github.com/Sbavdik/react-movies' target="_blank" rel="noreferrer" className='brand-logo'>React Movies</a>
+                <ul id='nav-mobile' className='right hide-on-med-and-down'>
+                    <li><a href='!#'>Repo</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+
+}
+
+export default Header;
